76a466d49c27baaab6b415b56bcabd3347ead2d5
Devang Saxena <dev62767@gmail.com>
2026-05-13 15:03:15 +0530

    fixed missing Mynotes import

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Register from './pages/Register'
 import Login from './pages/Login'
 import Notes from './pages/Notes'
-import Mynotes from './pages/Mynotes'
 const App = () => {
   return (
     <Router>
@@ -11,7 +10,7 @@ const App = () => {
         <Route path='/register' element={<Register />} />
         <Route path='/' element={<Login />} />
         <Route path='/notes' element={<Notes />} />
-        {/* <Route  path='/mynotes' element={<Mynotes />} /> */}
+        
       </Routes>
         
       
